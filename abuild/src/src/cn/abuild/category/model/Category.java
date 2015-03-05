3877142d43f18d9c33033277215c18d5367ae26c
suyear <suyear@qq.com>
2015-03-05 15:23:14 +0800

    up

--- abuild/src/src/cn/abuild/category/model/Category.java
+++
@@ -1,7 +0,0 @@
-package cn.abuild.category.model;
-
-import cn.abuild.category.model.original.CategoryAbstract;
-
-public class Category extends CategoryAbstract{
-
-}
--- abuild/src/src/cn/abuild/member/model/Member.java
+++
@@ -1,8 +0,0 @@
-package cn.abuild.member.model;
-
-import cn.abuild.member.model.original.MemberAbstract;
-
-
-public class Member  extends MemberAbstract{
-
-}
